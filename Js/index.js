494426a59836c46db07abd628a047cf483b99643
fasih ahmed khan <fasihkhan124124@gamil.com>
2022-09-27 19:28:20 +0500

    Done js for rps-ui

--- Js/index.js
+++ Js/index.js
@@ -2,6 +2,40 @@
 let computer = 0;
 let player = 0;
 
+// varables
+
+const title = document.querySelector(".title");
+const playerscore = document.querySelector(".playerScore");
+const computerscore = document.querySelector(".computerScore");
+const reset = document.querySelector(".reset");
+const options = document.querySelectorAll(".option");
+const winner = document.querySelector(".Winner");
+
+options.forEach((option) => {
+  option.addEventListener("click", () => {
+    let playerChoice = option.textContent;
+    let computerChoice = getComputerChoice();
+    title.textContent = playRound(playerChoice, computerChoice);
+    playerscore.textContent = player;
+    computerscore.textContent = computer;
+    if (player === 5) {
+      winner.textContent = "  🏆 player wins the Games";
+    } else if (computer === 5) {
+      winner.textContent = "  🏆 Computer wins the Games";
+    }
+
+    if (player === 6 || computer === 6) {
+      resetGame();
+    }
+    console.log(option.textContent);
+    console.log("check");
+    console.log(playerChoice);
+    console.log(computerChoice);
+    console.log(player);
+    console.log(computer);
+  });
+});
+
 // function to get random Cumputer Choice
 function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3) + 1;
@@ -14,61 +48,52 @@ function getComputerChoice() {
     : "none";
 }
 
-// // function to get Player Choice
-function getplayerChoice() {
-  let choice = prompt("Enter Your Choice(Rock,Paper,Scissors): ");
-  let firstLetter = choice.slice(0, 1).toUpperCase();
-  let otherLetters = choice.slice(1, choice.length).toLowerCase();
-  return (choice = `${firstLetter}${otherLetters}`);
-}
-
 // check who wins
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
-    console.log("Draw");
-    return 0;
+    // console.log("Draw");
+    return "Draw";
   } else {
     if (playerSelection === "Paper" && computerSelection === "Scissors") {
-      console.log("Computer Wins");
-      return ++computer;
+      // console.log("Computer Wins");
+      ++computer;
+      return "Computer Wins";
     } else if (playerSelection === "Paper" && computerSelection === "Rock") {
-      console.log("player Wins");
-      return ++player;
+      // console.log("player Wins");
+      ++player;
+      return "Player Wins";
     } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
-      console.log("player Wins");
-      return ++player;
+      // console.log("player Wins");
+      ++player;
+      return "Player Wins";
     } else if (playerSelection === "Rock" && computerSelection === "Paper") {
-      console.log("Computer Wins");
-      return ++computer;
+      // console.log("Computer Wins");
+      ++computer;
+      return "Computer Wins";
     } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
-      console.log("Computer Wins");
-      return ++computer;
+      // console.log("Computer Wins");
+      ++computer;
+      return "Computer Wins";
     } else if (
       playerSelection === "Scissors" &&
       computerSelection === "Paper"
     ) {
-      console.log("player Wins");
-      return ++player;
+      // console.log("player Wins");
+      ++player;
+      return "Player Wins";
     } else {
       console.log("invalid Choice");
       return 0;
     }
   }
 }
+// reset game
+reset.addEventListener("click", resetGame);
 
-function game() {
-  for (let i = 0; i < 20; i++) {
-    let playerChoice = getplayerChoice();
-    let computerChoice = getComputerChoice();
-    console.log(`player selects: ${playerChoice}`);
-    console.log(`computer selects: ${computerChoice}`);
-    playRound(playerChoice, computerChoice);
-    if (player === 5) {
-      alert("player wins the Games");
-      break;
-    } else if (computer === 5) {
-      alert("Computer wins the Games");
-      break;
-    }
-  }
+function resetGame() {
+  computer = 0;
+  player = 0;
+  winner.textContent = "";
+  playerscore.textContent = player;
+  computerscore.textContent = computer;
 }
